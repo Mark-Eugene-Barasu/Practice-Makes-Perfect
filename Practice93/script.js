0cd4a13ecfd66ecb439e9813a71cf7dba7aaa15f
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2024-04-11 02:40:06 +0200

    w

---
+++ Practice93/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $("h3").click(function(){
+        //$(this).style.color = "red";
+        $("section").toggle();
+    })
+});
+
+// so basically here is the synthax
+
+{
+    $(document).ready(function(){
+
+
+        // then the jQuery code goes in here.
+        // lovely!! Isn't it?
+    });
+}
